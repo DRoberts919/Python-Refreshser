93dc96cd72717c7bfd5bebac7859207feede4ed4
Dylan <60489717+DRoberts919@users.noreply.github.com>
2022-04-04 16:22:08 -0600

    class variable added

--- clas-variables.py
+++ clas-variables.py
@@ -2,7 +2,11 @@
 
 
 
+from mimetypes import init
+
+
 class Employee:
+    raise_amount = 1.04
 
     def __init__(self,first,last,pay):
         self.first = first
@@ -12,6 +16,9 @@ class Employee:
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
 
 
 
@@ -24,8 +31,6 @@ emp_2 = Employee('test', 'User', 60000)
 # print(emp_2)
 
 
-print(emp_1.email)
-print(emp_2.email)
-
-print(emp_1.fullname())
-print(emp_2.fullname())
+print(emp_1.pay)
+emp_1.apply_raise()
+print(emp_1.pay)
