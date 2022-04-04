2ac3e7e58e88170a53e496fdb170fc70028fba95
Dylan <60489717+DRoberts919@users.noreply.github.com>
2022-04-04 16:37:42 -0600

    class method to raise amount

--- classmethods-and-staticmethods.py
+++ classmethods-and-staticmethods.py
@@ -2,7 +2,13 @@
 
 
 
+
+from email.policy import EmailPolicy
+
+
 class Employee:
+    raise_amount = 1.04
+    num_of_emps = 0
 
     def __init__(self,first,last,pay):
         self.first = first
@@ -10,8 +16,17 @@ class Employee:
         self.pay = pay
         self.email = first + '-' + last + '@company'
 
+        Employee.num_of_emps += 1
+
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amount(cls,amount):
+        cls.raise_amount = amount
 
 
 
@@ -23,9 +38,8 @@ emp_2 = Employee('test', 'User', 60000)
 # print(emp_1)
 # print(emp_2)
 
+Employee.set_raise_amount(1.05)
 
-print(emp_1.email)
-print(emp_2.email)
-
-print(emp_1.fullname())
-print(emp_2.fullname())
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
