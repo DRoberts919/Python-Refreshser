a5334bbf09a81960b8b52515de53e3742ce0b30e
Dylan <60489717+DRoberts919@users.noreply.github.com>
2022-04-04 14:44:49 -0600

    class made

---
+++ classes-and-instances.py
@@ -0,0 +1,26 @@
+# python obejct-oriented Programming. 
+
+
+from cgi import print_arguments
+
+
+class Employee:
+
+    def __init__(self,first,last,pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '-' + last + '@company'
+
+
+        
+
+emp_1 = Employee('corey','schafer',5000)
+emp_2 = Employee('test', 'User', 60000)
+
+# print(emp_1)
+# print(emp_2)
+
+
+print(emp_1.email)
+print(emp_2.email)
